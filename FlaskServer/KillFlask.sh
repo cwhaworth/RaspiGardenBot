0a1a718044493a39d62a80324536a16bb12a5bc3
Chris Haworth <cwhaworth@gmail.com>
2024-02-04 20:14:01 -0500

    repository housekeeping

--- FlaskServer/KillFlask.sh
+++
@@ -1,1 +0,0 @@
-pkill -9 -f "python3 /usr/bin/flask run --host=0.0.0.0"
--- FlaskServer/start.sh
+++
@@ -1,3 +0,0 @@
-export FLASK_APP=/var/www/FlaskServer/FlaskStart.py
-export FLASK_ENV=development
-flask run --host=0.0.0.0 &
