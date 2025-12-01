42791075e5bc7d09ec6f6c15abe060b14a569f4f
Chris Haworth <cwhaworth@gmail.com>
2025-11-30 23:46:41 -0500

    implementing admin page

--- FlaskStart.py
+++ FlaskStart.py
@@ -578,8 +578,8 @@ def admin():
 						user_tuple = (new_user, password_hash.decode('utf-8'), new_priv_level)
 						sqlModifyQuery(f'insert into users (username, password_hash, priv_level) values {user_tuple}')
 					return redirect(url_for('.admin')) 
-				case _:
-					return redirect(url_for('.admin'))
+				# case _:
+				# 	return redirect(url_for('.admin'))
 			# if 'logout' in request.form.keys():
 			# 	return logout()
 			# elif key.startswith("editUser_"):
