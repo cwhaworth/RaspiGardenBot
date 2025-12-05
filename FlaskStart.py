d1e17ac8281f8818c1d7668f648ea1379825cdcf
Chris Haworth <cwhaworth@gmail.com>
2025-12-05 00:21:40 -0500

    implement new API and begin scheduler addition

--- FlaskStart.py
+++ FlaskStart.py
@@ -96,7 +96,7 @@ def get_forecast():
 		f'hourly=temperature_2m,precipitation_probability,precipitation,cloud_cover&'
 		f'daily=precipitation_probability_max')
 	response = requests.request('GET', url)
-	print(f'{response.status_code}\n{response.json()}')
+	print(f'{url}\n{response.status_code}\n{response.json()}')
 	return response.json()
 
 def start_scheduler():
