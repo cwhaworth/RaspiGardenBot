8569198d7ff07a11ccacdd0939939b9b914f80bf
Chris Haworth <cwhaworth@gmail.com>
2025-12-06 15:36:50 -0500

    implementing fixing scheduler

--- FlaskStart.py
+++ FlaskStart.py
@@ -10,7 +10,7 @@ Initialize - allows user to initialize the parameters the irriagtion system oper
 Water Log - Displays logs for data on what was watered, and when, as well as errors.
 '''
 
-import atexit, bcrypt, geopy, json, os, re, requests, sqlite3, time
+import atexit, bcrypt, geopy, json, os, re, requests, sqlite3, time, traceback
 import RPi.GPIO as GPIO
 from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import date, datetime
@@ -100,130 +100,133 @@ def update_last_rain(increment):
 	sqlModifyQuery(f'update system_params set val_num = ? where param = ?', update_tuple)
 
 def water_on_schedule():
-	now = datetime.now()
-	weather_resp = get_forecast(daily=False)
-	data = {
-			'use_api': bool(sqlSelectQuery('select val_bool from system_params where param = ?', ('use_api',))[0]),
-			'last_rain': sqlSelectQuery('select val_num from system_params where param = ?', ('last_rain',))[0],
-			'system_enable': bool(sqlSelectQuery('select val_bool from system_params where param = ?', ('system_enable',))[0]),
-			'cropData': sqlSelectQuery('select id, enabled, crop, pin, rain_inc from crops', fetchall=True),
-			'sysData': sqlSelectQuery('select * from system_temp', fetchall=True),
-			'weather': {
-				'units': {
-					'temp': f'{weather_resp["current_units"]["temperature_2m"]}',
-					'cloud_cover': f'{weather_resp["current_units"]["cloud_cover"]}',
-					'precipitation': f'{weather_resp["current_units"]["precipitation"][:2]}'
-				},
-				'current': {
-					'date': f'{now.date()}',
-					'time': f'{now.time()}'[:-7],
-					'temp': f'{weather_resp["current"]["temperature_2m"]}{weather_resp["current_units"]["temperature_2m"]}',
-					'cloud_cover': f'{weather_resp["current"]["cloud_cover"]}{weather_resp["current_units"]["cloud_cover"]}',
-					'precipitation': f'{weather_resp["current"]["precipitation"]}{weather_resp["current_units"]["precipitation"][:2]}'
-				},
-				'hourly': []
+	try:
+		now = datetime.now()
+		weather_resp = get_forecast(daily=False)
+		data = {
+				'use_api': bool(sqlSelectQuery('select val_bool from system_params where param = ?', ('use_api',))[0]),
+				'last_rain': sqlSelectQuery('select val_num from system_params where param = ?', ('last_rain',))[0],
+				'system_enable': bool(sqlSelectQuery('select val_bool from system_params where param = ?', ('system_enable',))[0]),
+				'cropData': sqlSelectQuery('select id, enabled, crop, pin, rain_inc from crops', fetchall=True),
+				'sysData': sqlSelectQuery('select * from system_temp', fetchall=True),
+				'weather': {
+					'units': {
+						'temp': f'{weather_resp["current_units"]["temperature_2m"]}',
+						'cloud_cover': f'{weather_resp["current_units"]["cloud_cover"]}',
+						'precipitation': f'{weather_resp["current_units"]["precipitation"][:2]}'
+					},
+					'current': {
+						'date': f'{now.date()}',
+						'time': f'{now.time()}'[:-7],
+						'temp': f'{weather_resp["current"]["temperature_2m"]}{weather_resp["current_units"]["temperature_2m"]}',
+						'cloud_cover': f'{weather_resp["current"]["cloud_cover"]}{weather_resp["current_units"]["cloud_cover"]}',
+						'precipitation': f'{weather_resp["current"]["precipitation"]}{weather_resp["current_units"]["precipitation"][:2]}'
+					},
+					'hourly': []
+				}
 			}
-		}
 
-	for i in range(0, len(weather_resp['hourly']['time'])):
-		t = datetime.strptime(weather_resp['hourly']['time'][i], "%Y-%m-%dT%H:%M")
-		if t >= now and len(weather_temp['hourly']) < 25:
+		for i in range(0, len(weather_resp['hourly']['time'])):
+			t = datetime.strptime(weather_resp['hourly']['time'][i], "%Y-%m-%dT%H:%M")
+			if t >= now and len(weather_temp['hourly']) < 25:
 
-			data['weather']['hourly'].append({
-				'date': t.date(),
-				'time': t.time(),
-				'temp': (f'{weather_resp["hourly"]["temperature_2m"][i]}'
-						f'{weather_resp["hourly_units"]["temperature_2m"]}'),
-				'cloud_cover': (f'{weather_resp["hourly"]["cloud_cover"][i]}'
-								f'{weather_resp["hourly_units"]["cloud_cover"]}'),
-				'precipitation_probability': (f'{weather_resp["hourly"]["precipitation_probability"][i]}'
-											f'{weather_resp["hourly_units"]["precipitation_probability"]}'),
-				'precipitation': (f'{weather_resp["hourly"]["precipitation"][i]} '
-								f'{weather_resp["hourly_units"]["precipitation"][:2]}')
-			})
-	percentRain = 0
-	avgPercentRain = 0
-	aboveFiddy = False
-	for hour in data['weather']['hourly']:
-		percentRain += hour['precipitation_probability']
-		if hour['precipitation_probability'] > 50:
-			aboveFiddy = True
-	if percentRain != 0 and len(data['weather']['hourly']) > 0:
-		avgPercentRain = percentRain / len(data['weather']['hourly'])
+				data['weather']['hourly'].append({
+					'date': t.date(),
+					'time': t.time(),
+					'temp': (f'{weather_resp["hourly"]["temperature_2m"][i]}'
+							f'{weather_resp["hourly_units"]["temperature_2m"]}'),
+					'cloud_cover': (f'{weather_resp["hourly"]["cloud_cover"][i]}'
+									f'{weather_resp["hourly_units"]["cloud_cover"]}'),
+					'precipitation_probability': (f'{weather_resp["hourly"]["precipitation_probability"][i]}'
+												f'{weather_resp["hourly_units"]["precipitation_probability"]}'),
+					'precipitation': (f'{weather_resp["hourly"]["precipitation"][i]} '
+									f'{weather_resp["hourly_units"]["precipitation"][:2]}')
+				})
+		percentRain = 0
+		avgPercentRain = 0
+		aboveFiddy = False
+		for hour in data['weather']['hourly']:
+			percentRain += hour['precipitation_probability']
+			if hour['precipitation_probability'] > 50:
+				aboveFiddy = True
+		if percentRain != 0 and len(data['weather']['hourly']) > 0:
+			avgPercentRain = percentRain / len(data['weather']['hourly'])
 
-	#perform, and log actions
-	if data['system_enable'] == False:
-		insertLogMessage("Did not water plants. Water system not enabled.")
+		#perform, and log actions
+		if data['system_enable'] == False:
+			insertLogMessage("Did not water plants. Water system not enabled.")
 
-	#if it rains: reset last-rained, and write to log
-	elif (aboveFiddy and avgPercentRain >= 4.16) or avgPercentRain > 50:
-		insertLogMessage(f'Did not water plants due to expected rain in the next 24 hours.'
-						f'Any hour above 50%: {"Yes" if aboveFiddy else "No"},' 
-						f'Average Percent Chance: {avgPercentRain}%.')
-		update_last_rain(0)
-
-	#If system is enabled, and API data is not in use OR if it does not rain: 
-	#water crops based on interval
-	elif data['use_api'] == False or (not aboveFiddy and avgPercentRain < 4.16) or avgPercentRain <= 50:
-		update_last_rain(data["last_rain"] + 1)
-		line = "Watered crops(s): "
-
-		pump = data['pump_pin']
-		valve_enable_pin = data['valve_enable_pin']
-		valve_open_pin = data['valve_open_pin']
-		valve_close_pin = data['valve_close_pin']
-
-		#start watering
-		#setup pins for pump and solenoid controller power
-		# GPIO.setup(pump, GPIO.OUT)
-		# GPIO.setup(valve_enable_pin, GPIO.OUT)
-		# GPIO.setup(valve_open_pin, GPIO.OUT)
-		# GPIO.setup(valve_close_pin, GPIO.OUT)
-		# valve = GPIO.PWM(valve_enable_pin, 100) #pin, and Hz
-
-		#turn on pump
-		# GPIO.output(pump, GPIO.HIGH)
-		time.sleep(data['delay_before'])
-		for crop in data["crop_data"]:
-			if crop[4] <= data["last_rain"] and data["last_rain"] % crop[4] == 0 and crop[1]:
-				# GPIO.setup(crop[3], GPIO.OUT)
-				# GPIO.output(crop[3], GPIO.LOW)
-				line += f"\"{crop[2]}\", "
-		valve.start(100) #duty cycle
-		# GPIO.output(valve_open_pin, GPIO.HIGH)
-		# GPIO.output(valve_close_pin, GPIO.LOW)
-		time.sleep(data['water_time'])
-
-		'''
-		end watering
-		1. clean up pump output
-		2. wait for configured after water operation delay
-		3. clean up solenoid and relay output 
-		'''
-		# GPIO.cleanup(pump)
-		time.sleep(data['delay_after'])
-		# GPIO.cleanup(valve_enable_pin)
-		# GPIO.cleanup(valve_open_pin)
-		# GPIO.cleanup(valve_close_pin)
-		for crop in data['crop_data']:
-			if crop[4] <= data['last_rained'] and data['last_rain'] % crop[4] == 0 and crop[1]:
-				# GPIO.cleanup(crop[3])
-				continue
-
-		#if API not in use and 30 days have passed, reset last rained
-		if data['use_api'] == False and data['last_rain'] == 30:
+		#if it rains: reset last-rained, and write to log
+		elif (aboveFiddy and avgPercentRain >= 4.16) or avgPercentRain > 50:
+			insertLogMessage(f'Did not water plants due to expected rain in the next 24 hours.'
+							f'Any hour above 50%: {"Yes" if aboveFiddy else "No"},' 
+							f'Average Percent Chance: {avgPercentRain}%.')
 			update_last_rain(0)
 
-		#prepare to write log message to 30 day, and 60 day log files
-		line = line[ : -2]
-		insertLogMessage(line)
+		#If system is enabled, and API data is not in use OR if it does not rain: 
+		#water crops based on interval
+		elif data['use_api'] == False or (not aboveFiddy and avgPercentRain < 4.16) or avgPercentRain <= 50:
+			update_last_rain(data["last_rain"] + 1)
+			line = "Watered crops(s): "
 
-	#if get forecast operation returned erroneous
-	else:
-		update_last_rain(data["last_rain"] + 1)
-		insertLogMessage("An error occurred during watering operations.")
-	
-	print(f'Ran water_on_schedule() at {str(now)}')
+			pump = data['pump_pin']
+			valve_enable_pin = data['valve_enable_pin']
+			valve_open_pin = data['valve_open_pin']
+			valve_close_pin = data['valve_close_pin']
+
+			#start watering
+			#setup pins for pump and solenoid controller power
+			# GPIO.setup(pump, GPIO.OUT)
+			# GPIO.setup(valve_enable_pin, GPIO.OUT)
+			# GPIO.setup(valve_open_pin, GPIO.OUT)
+			# GPIO.setup(valve_close_pin, GPIO.OUT)
+			# valve = GPIO.PWM(valve_enable_pin, 100) #pin, and Hz
+
+			#turn on pump
+			# GPIO.output(pump, GPIO.HIGH)
+			time.sleep(data['delay_before'])
+			for crop in data["crop_data"]:
+				if crop[4] <= data["last_rain"] and data["last_rain"] % crop[4] == 0 and crop[1]:
+					# GPIO.setup(crop[3], GPIO.OUT)
+					# GPIO.output(crop[3], GPIO.LOW)
+					line += f"\"{crop[2]}\", "
+			valve.start(100) #duty cycle
+			# GPIO.output(valve_open_pin, GPIO.HIGH)
+			# GPIO.output(valve_close_pin, GPIO.LOW)
+			time.sleep(data['water_time'])
+
+			'''
+			end watering
+			1. clean up pump output
+			2. wait for configured after water operation delay
+			3. clean up solenoid and relay output 
+			'''
+			# GPIO.cleanup(pump)
+			time.sleep(data['delay_after'])
+			# GPIO.cleanup(valve_enable_pin)
+			# GPIO.cleanup(valve_open_pin)
+			# GPIO.cleanup(valve_close_pin)
+			for crop in data['crop_data']:
+				if crop[4] <= data['last_rained'] and data['last_rain'] % crop[4] == 0 and crop[1]:
+					# GPIO.cleanup(crop[3])
+					continue
+
+			#if API not in use and 30 days have passed, reset last rained
+			if data['use_api'] == False and data['last_rain'] == 30:
+				update_last_rain(0)
+
+			#prepare to write log message to 30 day, and 60 day log files
+			line = line[ : -2]
+			insertLogMessage(line)
+
+		#if get forecast operation returned erroneous
+		else:
+			update_last_rain(data["last_rain"] + 1)
+			insertLogMessage("An error occurred during watering operations.")
+		
+		print(f'Ran water_on_schedule() at {str(now)}')
+	except exception as e:
+		print(f'Ran into an error while running water_on_schedule() at {str(now)}\ntraceback:\n{traceback.print_exc()}')
 
 def get_system_temp():
 	cpu = CPUTemperature()
