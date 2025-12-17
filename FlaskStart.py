499e5bbc5fdc79729f0da654639c95d9f61d0797
Chris Haworth <cwhaworth@gmail.com>
2025-12-17 00:56:15 -0500

    reenable GPIO

--- FlaskStart.py
+++ FlaskStart.py
@@ -543,7 +543,7 @@ def index():
 				},
 				'hourly': []
 			}
-			print(f'{json.dumps(data, indent = 2)}')
+			# print(f'{json.dumps(data, indent = 2)}')
 			for i in range(0, len(weather_resp['daily']['time'])):
 				t = datetime.strptime(weather_resp['daily']['time'][i], "%Y-%m-%d")
 				if t.date() == now.date():
